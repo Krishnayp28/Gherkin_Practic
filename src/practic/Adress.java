2d12f53c24ca54bda6ddc567a2e04de9a0c837bd
jignaypatel <jignayp@yahoo.co.uk>
2023-03-08 17:03:42 +0000

    Gherkin_Practic

---
+++ src/practic/Adress.java
@@ -0,0 +1,14 @@
+package practic;
+
+public class Adress {
+
+    public static void main(String [] args){
+
+        System.out.println("Jigna Patel");
+        System.out.println("26 Addison Way");
+        System.out.println("Northwood");
+        System.out.println("HA6 1SY");
+        
+    }
+
+}
